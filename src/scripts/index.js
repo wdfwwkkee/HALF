a4c8da66c0da6435b3d9518a5d1f852c1d059e3d
wdfwwkkee <denis.usmanov101@mail.ru>
2023-12-03 22:58:43 +0500

    asd

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,39 @@
+const signInBtn = document.getElementById('singIn');
+const singModal = document.getElementById('singModal');
+const closeSingInBtn = document.getElementById('closeSingIn');
+const tabContent = document.querySelectorAll('.tab-content');
+const tabBtn = document.querySelectorAll('.tabBtn');
+
+
+signInBtn.addEventListener('click', ()=> {
+  singModal.classList.add('open');
+})
+
+
+closeSingInBtn.addEventListener('click', ()=> {
+  singModal.classList.remove('open');
+})
+
+
+
+tabBtn.forEach((tab, index)=> {
+  tab.addEventListener('click', (e)=> {
+    tabBtn.forEach(tab=> {tab.classList.remove('active')});
+    tab.classList.add('active');
+    tabContent.forEach(content=>{content.classList.remove('active')});
+    tabContent[index].classList.add('active');
+  });
+})
+
+
+
+
+
+
+
+
+
+//hero-swiper
 var heroSwiper = new Swiper(".heroSwiper", {
     pagination: {
         el: ".swiper-pagination",
@@ -9,6 +45,7 @@ var heroSwiper = new Swiper(".heroSwiper", {
       },
 });
 
+//rait-swiper
 
 var raitSwiper = new Swiper(".raitingSwiper", {
     slidesPerView: "2",
