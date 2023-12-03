b48f8e7f4715a5e8b24e13d5eb25d19666b41120
wdfwwkkee <denis.usmanov101@mail.ru>
2023-12-03 21:16:08 +0500

    fix-close-btn

--- src/scripts/photograph-admin.js
+++ src/scripts/photograph-admin.js
@@ -1,14 +1,31 @@
+const body = document.getElementById('body');
+
 const showCompWorks = document.getElementById('show-completed-orders');
 const orderModal = document.querySelector('.completed-orders-modal');
-const closeBtn = document.getElementById('close-modal');
-const body = document.getElementById('body');
+const closeOrderModal = document.getElementById('close-order-modal');
+
+
+const closeGalleryBtn = document.getElementById('close-gallery-modal');
+const allWorksBtn = document.getElementById('all-works');
+const allWorksModal = document.querySelector('.all-works-modal');
+
+closeGalleryBtn.addEventListener('click', ()=> {
+    allWorksModal.classList.remove('open');
+    body.classList.remove('lock');
+})
+
+allWorksBtn.addEventListener('click', ()=> {
+    allWorksModal.classList.add('open');
+    body.classList.add('lock');
+})
+
+
 showCompWorks.addEventListener('click', ()=> {
     orderModal.classList.add('open');
     body.classList.add('lock');
 });
 
-closeBtn.addEventListener('click', ()=> {
+closeOrderModal.addEventListener('click', ()=> {
     orderModal.classList.remove('open');
     body.classList.remove('lock');
-
 })
